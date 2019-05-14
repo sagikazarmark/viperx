208aa32eaffe8c8d681f89dad8c04fd7eae3344d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-05-14 11:46:01 +0200

    Add bankvaults remote provider

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module github.com/sagikazarmark/viperx
 go 1.12
 
 require (
-	github.com/hashicorp/vault/api v1.0.2
+	github.com/banzaicloud/bank-vaults v0.0.0-20190514090641-f54a803d571a
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/hashicorp/vault v1.1.2
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/viper v1.3.2
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
+	golang.org/x/sys v0.0.0-20190403152447-81d4e9dc473e // indirect
+	golang.org/x/text v0.3.1-0.20181227161524-e6919f6577db // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
 )
---
+++ remote/bankvaults/provider_test.go
@@ -0,0 +1,80 @@
+package bankvaults
+
+import (
+	"net/url"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/vault/api"
+	"github.com/spf13/viper"
+)
+
+func TestConfigProvider(t *testing.T) {
+	address := os.Getenv("VAULT_ADDR")
+	if address == "" {
+		t.Skip("VAULT_ADDR environment variable not found")
+	}
+
+	token := os.Getenv("VAULT_TOKEN")
+	if token == "" {
+		t.Skip("VAULT_TOKEN environment variable not found")
+	}
+
+	config := api.DefaultConfig()
+	config.Address = address
+	client, err := api.NewClient(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client.SetToken(token)
+
+	remoteConfig := viper.RemoteConfig
+	defer func() {
+		viper.RemoteConfig = remoteConfig
+	}()
+
+	supportedRemoteProviders := viper.SupportedRemoteProviders
+	defer func() {
+		viper.SupportedRemoteProviders = supportedRemoteProviders
+	}()
+
+	viper.RemoteConfig = NewConfigProvider()
+	viper.SupportedRemoteProviders = append(viper.SupportedRemoteProviders, "bankvaults")
+
+	_, err = client.Logical().Write("secret/data/hello", map[string]interface{}{
+		"data": map[string]interface{}{
+			"database": map[string]interface{}{
+				"user": "root",
+				"pass": "root",
+			},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	u, err := url.Parse(address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	u.RawQuery = "token=" + token
+
+	v := viper.New()
+	_ = v.AddRemoteProvider("bankvaults", u.String(), "secret/data/hello")
+	v.SetConfigType("json")
+
+	err = v.ReadRemoteConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if v.GetString("database.user") != "root" || v.GetString("database.pass") != "root" {
+		t.Errorf("failed to read secrets from vault")
+	}
+
+	_, err = client.Logical().Delete("secret/metadata/hello")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
